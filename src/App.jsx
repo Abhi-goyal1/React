3c0b55493e10140383c7afe55310cd45806d16f2
abhi-goyal1 <Abhigoyal72621289@gmail.com>
2024-08-26 21:38:33 +0530

    more

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@
 import './App.css'
 
 function App() {
-  
-
   return (
     <>
       <h1>Hello World</h1>
-      <h1>This is my first React app</h1>
+      <button>like</button>
       <h1>this is second time connect</h1>
+    </>
+  )
+}
+function more() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button>like</button>
       <h1>this is second time connect</h1>
     </>
   )
 }
 
-export default App
+
+
+export default App;  more;
